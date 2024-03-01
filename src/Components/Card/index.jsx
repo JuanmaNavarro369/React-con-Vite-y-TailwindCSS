1282fbf83e045950ad629ff4cc0306e51618a625
Juan Manuel Navarro González <juanma_2_@hotmail.com>
2024-03-01 19:29:18 +0100

    Evitar productos duplicados en el carrito (My Order)

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,4 +1,4 @@
-import { PlusCircleIcon } from "@heroicons/react/24/solid"
+import { PlusCircleIcon, CheckIcon } from "@heroicons/react/24/solid"
 import { useContext } from "react";
 import { MyContext } from "../../Context"
 
@@ -13,42 +13,59 @@ function Card(data) {
         setProductToShow(productData)
     }
 
-    return(
-    <div
-    className="bg-white cursor-pointer w-56 h-60 p-3"
-    onClick={() => {
-        showProduct(data.data)
-        closeMyOrder()
-        }}>
+    const renderIcon = (id) => {
+        const isInCart = addCartProducts.filter(product => product.id === id).length > 0
 
-        <figure className="relative mb-2 w-full h-4/5">
-            <span className="absolute bottom-1 left-1 rounded-lg bg-white/60 text-sm px-1 py-0.5">{data.data.category.name}</span>
-            <img
-            src={data.data.image}
-            alt={data.data.title}
-            className="w-full h-full rounded-lg object-contain"
-            // Para hacer que la imagen conserve su relación de aspecto se usa la
-            // propiedad 'object'
-            />
-            <PlusCircleIcon
-            className="absolute text-sm top-1 right-1 flex justify-center items-center bg-white/90 rounded-full w-6 h-6"
-            onClick={(event) => {
-                // stopPropagation evita el conflicto de eventos entre mostrar Product Detail y MyOrderMenu
+        if (isInCart) {
+            return (
+                <div className="bg-green-600 w-6 h-6 absolute top-1 right-1 flex justify-center items-center rounded-full">
+                    <CheckIcon
+                    className="text-white"/>
+                </div>
+                )}
+        else {
+            return(
+                <PlusCircleIcon
+                className="absolute text-sm top-1 right-1 flex justify-center items-center bg-white/90 rounded-full w-6 h-6"
+                onClick={(event) => {
+                //stopPropagation evita el conflicto de eventos entre mostrar Product Detail y MyOrderMenu
                 event.stopPropagation()
                 setCount(count + 1)
                 setAddCartProducts([...addCartProducts, data.data])
                 openMyOrder()
                 closeProductDetail()
                 }}
-            />
-        </figure>
-        <p className="flex justify-between">
-            <span className="text-sm font-semibold">{data.data.title}</span>
-            <span className="mr-2 text-lg font-bold">${data.data.price}</span>
-        </p>
+                />
+            )}
+        }
 
-    </div>
-    )
+    return(
+    <div
+        className="bg-white cursor-pointer w-56 h-60 p-3"
+        onClick={() => {
+            showProduct(data.data)
+            closeMyOrder()
+            }}>
+
+            <figure className="relative my-6 w-full h-4/5">
+                <span className="absolute bottom-1 left-1 rounded-lg bg-white/60 text-sm px-1 py-0.5">
+                    {data.data.category}
+                </span>
+                <img
+                src={data.data.image}
+                alt={data.data.title}
+                className="w-full h-full rounded-lg object-contain"
+                // Para hacer que la imagen conserve su relación de aspecto se usa la propiedad 'object'
+                />
+                {renderIcon(data.data.id)}
+            </figure>
+            <p className="flex justify-between">
+                <span className="text-sm font-semibold">{data.data.title}</span>
+                <span className="mx-3 text-lg font-bold">${data.data.price}</span>
+            </p>
+
+        </div>
+        )
 }
 
 export { Card };
